d2ddc8e324cd806c81712241d49c03026c034c56
Jayson <oliciajayson@gmail.com>
2023-05-09 05:34:27 +0800

    add social links

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
-
+import SocialLink from "./components/SocialLink";
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+
+      <SocialLink />
     </div>
   );
 }
